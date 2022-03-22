43748d56c1d00823ed1c1069fc2a51341815ebe9
Tdazle007 <91950836+Tdazle007@users.noreply.github.com>
2022-03-23 00:57:38 +0100

    Add files via upload

---
+++ src/Entity/Product.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $sku;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $category;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(?string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getFinal()
+    {
+        $val = $this->getPrice();
+        $cat = $this->getCategory();
+        if ($cat == "boots"){$final = $val * 30 /100;}else{$final = $val;}
+        return $final;
+    }
+
+    public function getDiscountPercentage()
+    {
+        $cat = $this->getCategory();
+        if ($cat == "boots"){$percentage = "30%";}else{$percentage = null;}
+        return $percentage;
+    }
+
+    public function getCurrency()
+    {
+        $currency = "EUR";
+        return $currency;
+    }
+}
